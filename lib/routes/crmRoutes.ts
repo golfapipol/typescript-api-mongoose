93bf600201bb9e8468bc4b60599412ef1f5ba384
Apipol Sukgler <golf.apipol@gmail.com>
2018-12-09 22:14:50 +0700

    STEP 3: add CRUD for contact

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -1,13 +1,26 @@
 import {Request, Response} from 'express';
 import express from 'express';
 
+import { ContactController } from './../controllers/crmController';
+
 export class Routes {
+    public contactController: ContactController = new ContactController();
+    
     public routes(app: express.Application): void {
         app.route('/')
             .get((req:Request, res: Response) => {
                 res.status(200).send({
                     message: 'GET request successful!!!!'
                 })     
-            })
+            });
+
+        app.route('/contact')
+            .get(this.contactController.getContacts)
+            .post(this.contactController.addNewContact);
+
+        app.route('/contact/:contactId')
+            .get(this.contactController.getContactWithID)
+            .put(this.contactController.updateContact)
+            .delete(this.contactController.deleteContact)
     }
 }
